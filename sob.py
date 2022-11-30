cbc5df0a38ae21684412ed6eda652d4ab5b7a402
08-Ahlaqul-Karimah <83933498+08-Ahlaqul-Karimah@users.noreply.github.com>
2022-11-30 22:33:52 +0700

    Update sob.py

--- sob.py
+++ sob.py
@@ -37,7 +37,7 @@ with upload_data:
     uploaded_files = st.file_uploader("Upload file CSV", accept_multiple_files=True)
     for uploaded_file in uploaded_files:
         df = pd.read_csv(uploaded_file)
-        st.write("Nama File Anda = ", uploaded_file.name)
+        st.write("Nama File Anda = ", uploaded_file.name) 
         st.dataframe(df)
 
 with deskripsi:
